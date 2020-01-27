b4ffd6e571fbfdddc88691bed4179f568e535779
Nesquick <dupuythibaut@yahoo.fr>
2020-01-27 19:18:09 +0100

    Création des vues partielles

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -4,16 +4,16 @@ class Pages extends CI_Controller{
 
 	function view($page = 'home')
 	{
-		if( !file_exists('application/views/pages/'.$page.'.php'))
+		if( !file_exists('application/views/Pages/'.$page.'.php'))
 		{
 			show_404();
 		}
-
-		$this->load->view('pages/'.$page);
-
+    $this->load->view('Partials/header.php');
+    $this->load->view('Partials/navmenu.php');
+		$this->load->view('Pages/'.$page);
+    $this->load->view('Partials/footer.php');
 	}
 
 }
-
 
 ?>
