a9de78a7904c9448ec2780ad33937fda869b5498
tdupuy <tdupuy@learnatech.fr>
2020-01-28 16:46:44 +0100

    Fonctionnement sur Linux + droits

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,18 +1,17 @@
 <?php
 
 class Pages extends CI_Controller{
-
 	function view($page = 'home')
 	{
 		if( !file_exists('application/views/Pages/'.$page.'.php'))
 		{
 			show_404();
 		}
-    $this->load->helper('url');
-    $this->load->view('Partials/header.php');
-    $this->load->view('Partials/navmenu.php');
+    	$this->load->helper('url');
+	    $this->load->view('Partials/header.php');
+	    $this->load->view('Partials/navmenu.php');
 		$this->load->view('Pages/'.$page);
-    $this->load->view('Partials/footer.php');
+	    $this->load->view('Partials/footer.php');
 	}
 
 }
