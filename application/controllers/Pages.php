e08e42f4a6cf4de668f9e2dadc615bfcab33c7a4
Tdupuy <tdupuy@learnatech.fr>
2020-01-29 08:43:48 +0100

    Création du modèle"serie"

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -7,7 +7,12 @@ class Pages extends CI_Controller{
 		{
 			show_404();
 		}
+		/*$data = file_get_contents("https://api.themoviedb.org/3/discover/tv?api_key=5b6d508b82c80bff9bf92f391b6d9d4f&language=fr-FR&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false");
+		var_dump(json_decode($data));
+		die();*/
 
+		$this->load->model('Series/Series_model');
+		var_dump($this->Series_model->get_random_series());die();
 		$data_header = [];
 		$data_navmenu = [];
 		$data_footer = [];
