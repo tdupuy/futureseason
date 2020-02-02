824f4d33428b14a61470589b192133f9475065f9
Nesquick <dupuythibaut@yahoo.fr>
2020-02-02 15:41:57 +0100

    Version "propre"

--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -7,24 +7,28 @@
           <center><h2 class="display-3 page-title">{heading}</h2></center>
         </div>
       </div>
-
-
-
-   <main role="main">
-	 <div class="container">
+	     <div id="cat-1" class="container py-5">
         <!-- Example row of columns -->
-        <div class="row mt-5 justify-content-center">
+        <div class="row mt-5 ml-5 "><!--d-flex flex-row flex-nowrap-->
           {series}
           <div class="card card-custom mx-2 mb-3" style="width: 18rem;">
             <img class="card-img-top" src="{img_path}" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">{name}</h5>
               <p class="card-text">{overview}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">{production}</li>
+              <li class="list-group-item">{current_season}</li>
+              <li class="list-group-item">{type}</li>
+            </ul>
+            <div class="card-body">
               <a href="#" class="btn btn-primary">Suivre</a>
             </div>
           </div>
           {/series}
         </div>
+      </div>
 		    <hr>
 
     </main>
